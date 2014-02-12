f3f1e2adbc6ac70fe01140983234e0d5eb2325d3
Jason R. Coombs <jaraco@jaraco.com>
2014-02-12 16:27:43 -0500

    Make sure to unconfigure

--- motivation/conftest.py
+++ motivation/conftest.py
@@ -7,9 +7,12 @@ pmxbot.karma = mock.MagicMock()
 
 def pytest_configure(config):
 
-	patcher = mock.patch.dict('sys.modules', {
+	config.patcher = mock.patch.dict('sys.modules', {
 		'pmxbot': pmxbot,
 		'pmxbot.core': pmxbot.core,
 		'pmxbot.karma': pmxbot.karma,
 	})
-	patcher.__enter__()
+	config.patcher.__enter__()
+
+def pytest_unconfigure(config):
+	config.patcher.__exit__(None, None, None)
