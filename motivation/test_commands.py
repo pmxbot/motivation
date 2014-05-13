24cb8fd2c627e9f764f1e334d592dc323b001f10
Jason R. Coombs <jaraco@jaraco.com>
2014-05-13 09:09:39 -0400

    Added test for Schneier

--- motivation/test_commands.py
+++ motivation/test_commands.py
@@ -5,3 +5,7 @@ from motivation import commands
 def test_jm():
 	res = commands.jm(None, None, "#inane", None, "")
 	assert isinstance(res, six.text_type)
+
+def test_schneier():
+	res = commands.schneier(None, None, "#inane", None, "foo")
+	assert 'foo' in res
